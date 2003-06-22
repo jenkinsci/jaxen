10aba56c0a53ad3d947dcd6006833956fe5aed81
proyal <null>
2003-06-22 18:55:00 +0000

    Remove unused import :)

--- src/java/main/org/jaxen/jdom/DocumentNavigator.java
+++ src/java/main/org/jaxen/jdom/DocumentNavigator.java
@@ -1,7 +1,7 @@
 /*
  * $Header$
- * $Revision: 270 $
- * $Date: 2002-05-20 01:34:12 -0700 (Mon, 20 May 2002) $
+ * $Revision: 314 $
+ * $Date: 2003-06-22 11:55:00 -0700 (Sun, 22 Jun 2003) $
  *
  * ====================================================================
  *
@@ -56,7 +56,7 @@
  * James Strachan <jstrachan@apache.org>.  For more information on the 
  * Jaxen Project, please see <http://www.jaxen.org/>.
  * 
- * $Id: DocumentNavigator.java 270 2002-05-20 08:34:12Z jstrachan $
+ * $Id: DocumentNavigator.java 314 2003-06-22 18:55:00Z proyal $
  */
 
 
@@ -78,7 +78,6 @@ import org.jdom.Attribute;
 import org.jdom.CDATA;
 import org.jdom.ProcessingInstruction;
 import org.jdom.Namespace;
-import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
 
 import java.util.HashMap;
