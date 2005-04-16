429f6f26d1df4851bf35afe36c43e353f82c6cff
elharo <null>
2005-04-16 14:18:12 +0000

    Using NumberFormat rather than Double.valueOf to convert doubles to strings. This avoids the use of exponential notation.

--- src/java/main/org/jaxen/function/StringFunction.java
+++ src/java/main/org/jaxen/function/StringFunction.java
@@ -1,7 +1,7 @@
 /*
  * $Header$
- * $Revision: 633 $
- * $Date: 2005-04-16 06:45:12 -0700 (Sat, 16 Apr 2005) $
+ * $Revision: 637 $
+ * $Date: 2005-04-16 07:18:12 -0700 (Sat, 16 Apr 2005) $
  *
  * ====================================================================
  *
@@ -56,7 +56,7 @@
  * James Strachan <jstrachan@apache.org>.  For more information on the 
  * Jaxen Project, please see <http://www.jaxen.org/>.
  * 
- * $Id: StringFunction.java 633 2005-04-16 13:45:12Z elharo $
+ * $Id: StringFunction.java 637 2005-04-16 14:18:12Z elharo $
  */
 
 
@@ -69,8 +69,10 @@ import org.jaxen.Navigator;
 import org.jaxen.UnsupportedAxisException;
 import org.jaxen.JaxenRuntimeException;
 
+import java.text.NumberFormat;
 import java.util.List;
 import java.util.Iterator;
+import java.util.Locale;
 
 /**
  * <p><b>4.2</b> <code><i>string</i> string(<i>object</i>)</code>
@@ -80,6 +82,13 @@ import java.util.Iterator;
 public class StringFunction implements Function
 {
     
+    private static NumberFormat format = NumberFormat.getInstance(Locale.ENGLISH);
+    
+    static {
+        format.setGroupingUsed(false);
+        format.setMaximumFractionDigits(32);
+    }
+
     public Object call(Context context,
                        List args) throws FunctionCallException
     {
@@ -183,28 +192,7 @@ public class StringFunction implements Function
 
     public static String stringValue(double value)
     {
-        if (Double.isNaN(value))
-        {
-            return "NaN";
-        }
-        if (0.0 == value)
-        {
-            return "0";
-        }
-        if (Double.isInfinite(value) && value < 0)
-        {
-            return "-Infinity";
-        }
-        if (Double.isInfinite(value) && value > 0)
-        {
-            return "Infinity";
-        }
-        if (((long) value) == value)
-        {
-            return Long.toString((long) value);
-        }
-        // ???? what about exponential notation????
-        return Double.toString(value);
+        return format.format(value);
     }
 
     public static String stringValue(boolean bool)
